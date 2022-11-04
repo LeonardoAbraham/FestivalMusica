6531db4c8294bc55877becfc3e3ae98ef5728821
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2022-11-04 10:59:45 -0600

    Añadiendo watch a Gulp SASS

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
-const { src, dest } = require("gulp");
+const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require('sass'));
 
 function css( done ) {
-    src('src/scss/app.scss') //Identificar el archivo SASS
+    src("src/scss/app.scss") //Identificar el archivo SASS
         .pipe( sass() ) //Compilarlo
         .pipe( dest("build/css") ) //Almacenar en el disco duro
 
     done(); //Callback que avisa a gulp cuando llegamos al final
 }
 
+function dev( done ) {
+    watch("src/scss/app.scss", css)
+    done();
+}
+
 /* npx gulp tarea */
 
 exports.css = css;
+exports.dev = dev;
 
