641cf5acddadd95ca5197096a6cf53b65007daed
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2022-11-23 11:53:38 -0600

    Generando el HTML de la galería

---
+++ build/js/app.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function(){
+    iniciarApp();
+});
+
+function iniciarApp() {
+    crearGaleria();
+}
+
+function crearGaleria () {
+    const galeria = document.querySelector('.galeria-imagenes');
+
+    galeria.textContent = "Vamos a crear la galeria";
+
+    for(let i = 1; i<=12; i++){
+        const imagen = document.createElement('picture');
+        imagen.innerHTML = `
+            <source srcset="build/img/thumb/${i}.avif" type="imagen/avif">
+            <source srcset="build/img/thumb/${i}.webp" type="imagen/webp">
+            <img loading="lazy" width="200" height="300" src="src/img/thumb/${i}.jpg" alt="Imagen Galeria">
+        `;
+        galeria.appendChild(imagen);
+    }
+}
