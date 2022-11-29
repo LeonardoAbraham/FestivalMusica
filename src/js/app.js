4b3421fbedd05a95174d68420a0419544015e4e3
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2022-11-29 10:34:36 -0600

    Imagen a la que le damos click para mostrar una versión grande

--- src/js/app.js
+++ src/js/app.js
@@ -16,6 +16,28 @@ function crearGaleria () {
             <source srcset="build/img/thumb/${i}.webp" type="imagen/webp">
             <img loading="lazy" width="200" height="300" src="src/img/thumb/${i}.jpg" alt="Imagen Galeria">
         `;
+
+        imagen.onclick = function(){
+            mostrarImagen(i);
+        }
+
         galeria.appendChild(imagen);
     }
+}
+
+function mostrarImagen(id) {
+    const imagen = document.createElement('picture');
+    imagen.innerHTML = `
+        <source srcset="build/img/grande/${id}.avif" type="imagen/avif">
+        <source srcset="build/img/grande/${id}.webp" type="imagen/webp">
+        <img loading="lazy" width="200" height="300" src="src/img/grande/${id}.jpg" alt="Imagen Galeria">
+    `;
+
+    const overlay = document.createElement('DIV');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+
 }
