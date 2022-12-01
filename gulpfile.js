38d97c54b31ffaf0ee32e1c8c4000fb415b458a4
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2022-11-30 18:08:12 -0600

    Mejorar Código JS con Terser y Gulp (minificar js)

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,9 @@ const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 const avif = require('gulp-avif');
 
+//Javascript
+const terser = require('gulp-terser');
+
 function css( done ) {
     src("src/scss/**/*.scss") //Identificar el archivo SASS
         .pipe(sourcemaps.init())
@@ -67,6 +70,9 @@ function versionAvif( done ){
 
 function javascript(done){
     src('src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/js'));
 
     done();
